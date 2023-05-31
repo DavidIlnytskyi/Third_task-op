a2cd647ed2c4a292584ab114759dc39880de803c
unknown <ilnytskyi.pn@ucu.edu.ua>
2023-05-31 15:32:12 +0300

    build tree updated, added some minor changes

--- board.py
+++ board.py
@@ -1,15 +1,20 @@
 '''Represents board for tic-tac-toe game'''
+from btree import BinaryTree
+from btnode import Node
+
 
 class Board():
     '''Represents a board in tic-tac-toe'''
     def __init__(self):
         self.board = [['' for i in range(3)] for i in range(3)]
+        self.previous_player = None
 
     def make_move(self, position, turn):
         '''Takes user move'''
         if self.board[position[0]][position[1]] != '':
             raise KeyError('This position is already used')
         self.board[position[0]][position[1]] = turn
+        self.previous_player = turn
 
     def __str__(self):
         '''Returns str representation of a board'''
@@ -50,11 +55,15 @@ class Board():
         for cord in [(row, col, self.board[row][col]) \
                      for row in range(3) for col in range(3) if self.board[row][col] != '']:
             res.board[cord[0]][cord[1]] = cord[2]
+        res.previous_player = self.previous_player
         return res
 
 if __name__ == "__main__":
     brd = Board()
     brd.make_move((1, 1), 'x')
+    tree = BinaryTree()
+    tree.build_tree(tree.root, brd)
+    tree.check()
     print(brd)
     print(brd.get_status())
     print(brd.clone_board())
--- btree.py
+++ btree.py
@@ -2,18 +2,38 @@
 from btnode import Node
 
 class BinaryTree():
-    def __init__(self, board=None):
+    '''Binary tree'''
+    def __init__(self):
         self.root = Node()
-        self.board = board
 
     def build_tree(self, root, board):
-        if root.board.get_status() == 'continue':
-            return None
-        elif root.board.get_status() != 'x' and root.board.get_status() != '0':
-            self.root.left = Node(board.get_available_moves()[0])
-            self.root.left.brd = board.clone_board()
-            self.root.left.brd = self.root.left.brd.make_move(board.get_available_moves()[0])
-            if len(board.get_available_moves()) != 1:
-                self.root.right = Node(board.get_available_moves()[0])
-                self.root.right.brd = board.clone_board()
-                self.root.right.brd = self.root.left.brd.make_move(board.get_available_moves()[0])
+        '''Builds a tree'''
+        if board.get_status() == 'x':
+            pass
+        elif board.get_status() == '0':
+            pass
+        elif board.get_status() == 'draw':
+            pass
+        else:
+            if len(board.get_available_moves()) < 2:
+                return
+            if len(board.get_available_moves()) > 2:
+                root.left = Node(board.get_available_moves()[0])
+                root.left.brd = board.clone_board()
+                if root.left.brd.previous_player == 'x':
+                    root.left.brd.make_move(root.left.turn_cord, 'o')
+                else:
+                    root.left.brd.make_move(root.left.turn_cord, 'x')
+
+                if len(board.get_available_moves()) != 2:
+                    self.build_tree(root.left, root.left.brd)
+
+                root.right = Node(board.get_available_moves()[1])
+                root.right.brd = board.clone_board()
+                if root.right.brd.previous_player == 'x':
+                    root.right.brd.make_move(root.right.turn_cord, 'o')
+                else:
+                    root.right.brd.make_move(root.right.turn_cord, 'x')
+
+                if len(board.get_available_moves()) != 2:
+                    self.build_tree(root.right, root.right.brd)
